1d7f01ec55d4d714cd4e49a63650181446608940
mattpollard0 <mattpollard0@gmail.com>
2023-07-26 09:02:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ let devsColors = [{
 
 function generateTorus() {
     let appTorus = document.querySelector('#torus');
-    let outputTorus = '';
+    let outputTorus = '<h2>Torus</h2>';
     for (var i = 0; i < torusColors.length; i++) {
         outputTorus += '<div class="color-card"><div class="color" style="background:' + torusColors[i].hex + '"></div><div class="color-info"><p class="color-name">' + torusColors[i].name + '</p><p class="color-hex" title="Copy" data-clipboard-text="' + torusColors[i].hex + '">' + torusColors[i].hex + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p><p title="Copy" class="color-rgb" data-clipboard-text="' + torusColors[i].rgb + '">' + torusColors[i].rgb + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p></div></div>';
     }
