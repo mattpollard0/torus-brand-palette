a4800a2fc5836cd44541a6be9f00ffdd68165159
Matt Pollard <mattpollard0@gmail.com>
2023-08-23 12:01:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -206,11 +206,12 @@ function generatePalette() {
     for (var i = 0; i < salesColors.length; i++) {
         outputSales += '<div class="color-card"><div class="color" style="background:' + salesColors[i].hex + '"></div><div class="color-info"><p class="color-name">' + salesColors[i].name + '</p><p class="color-hex" title="Copy" data-clipboard-text="' + salesColors[i].hex + '">' + salesColors[i].hex + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p><p title="Copy" class="color-rgb" data-clipboard-text="' + salesColors[i].rgb + '">' + salesColors[i].rgb + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p><p title="Copy" class="color-cmyk" data-clipboard-text="' + salesColors[i].cmyk + '">' + salesColors[i].cmyk + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p><p title="Copy" class="color-pantone" data-clipboard-text="' + salesColors[i].pantone + '">' + "Pantone " + salesColors[i].pantone + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p></div></div>';
     }
+    /*
     let appGreys = document.querySelector('#greys');
     let outputGreys = '';
     for (var i = 0; i < greysColors.length; i++) {
         greysColors += '<div class="color-card"><div class="color" style="background:' + greysColors[i].hex + '"></div><div class="color-info"><p class="color-name">' + greysColors[i].name + '</p><p class="color-hex" title="Copy" data-clipboard-text="' + greysColors[i].hex + '">' + greysColors[i].hex + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p><p title="Copy" class="color-rgb" data-clipboard-text="' + greysColors[i].rgb + '">' + greysColors[i].rgb + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p><p title="Copy" class="color-cmyk" data-clipboard-text="' + greysColors[i].cmyk + '">' + greysColors[i].cmyk + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p><p title="Copy" class="color-pantone" data-clipboard-text="' + greysColors[i].pantone + '">' + "Pantone " + greysColors[i].pantone + ' <i class="fa fa-clipboard" aria-hidden="true"></i></p></div></div>';
-    }
+    }*/
 
     appTorus.innerHTML = outputTorus;
     appHMS.innerHTML = outputHMS;
